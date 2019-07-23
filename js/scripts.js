42e5c819c63a945fa7b4b94ddcd84ebad074f3d0
Umwali Clara <umwaliclara@gmail.com>
2019-07-23 12:17:18 +0200

    adding a scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+});
+jQuery('p').click(function(){
+    alert("This is a paragraph")
+});
+jQuery('img').click(function(){
+    alert("This is an image")
+});
